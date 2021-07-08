b5f68e7d7c9f337f69a721d7891b6d05b2c1a05a
PrynsTag <201812311@fit.edu.ph>
2021-07-08 08:33:30 +0800

    Changed model name

--- application/models/Register_model.php
+++ application/models/Register_model.php
@@ -4,7 +4,7 @@
 class User_model extends CI_Model
 {
     public function insert_user($data){
-        $this->db->insert($data);
+        $this->db->insert("users", $data);
         return $this->db->insert_id();
     }
 }
