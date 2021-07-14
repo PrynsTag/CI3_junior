682c015cdbf6d8449a741b9234eecd0b22883f8b
PrynsTag <201812311@fit.edu.ph>
2021-07-14 20:30:26 +0800

    Added email verification

--- application/models/Register_model.php
+++ application/models/Register_model.php
@@ -4,7 +4,14 @@ class Register_model extends CI_Model
 {
     public function insert_user($data)
     {
-        $this->db->insert("users", $data);
+        $this->db->insert("user", $data);
         return $this->db->insert_id();
+    }
+
+    public function get_code($uri_code)
+    {
+        $query = $this->db->get_where("user", array("user_code" => $uri_code));
+        $result = $query->result();
+        return $result;
     }
 }
