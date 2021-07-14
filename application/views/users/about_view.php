aa31d687c682114eeb6d7471fa5f4f761903c22f
rckyrmnx <56989333+rckyrmnx@users.noreply.github.com>
2021-07-14 23:12:53 +0800

    Updated About View Details

--- application/views/users/about_view.php
+++ application/views/users/about_view.php
@@ -1,22 +1,32 @@
 <div class="aboutBody">
     <div class="team-section">
         <div class="inner-width">
-            <h1>Meet Our Team</h1>
+            <h1>Meet The Team</h1>
             <div class="pers">
                 <div class="pe">
-                    <img src="<?= base_url(); ?>assets/images/Ricky2.png" alt="">
-                    <div class="p-name">Ricky Martin Roman</div>
-                    <div class="p-des">Back-end Developer</div>
+                    <img src="<?= base_url(); ?>assets/images/Prince.png" alt="">
+                    <div class="p-name">Prince Carl Velasco</div>
+                    <div class="p-des">Leader</div>
                     <div class="p-sm">
-                        <a href="https://www.facebook.com/rckyrmn"><i class="fab fa-facebook-f"></i></a>
-                        <a href="https://twitter.com/rckyrmnx"><i class="fab fa-twitter"></i></a>
-                        <a href="https://www.instagram.com/rckyrmnx/"><i class="fab fa-instagram"></i></a>
+                        <a href="https://www.facebook.com/princecarl.velasco"><i class="fab fa-facebook-f"></i></a>
+                        <a href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
+                        <a href="https://www.instagram.com/prynstag/"><i class="fab fa-instagram"></i></a>
                     </div>
                 </div>
                 <div class="pe">
-                    <img src="<?= base_url(); ?>assets/images/Julie.jfif" alt="">
+                    <img src="<?= base_url(); ?>assets/images/JC.png" alt="">
+                    <div class="p-name">John Christian Garon</div>
+                    <div class="p-des">Member</div>
+                    <div class="p-sm">
+                        <a href="https://www.facebook.com/johnchristian.garon/"><i class="fab fa-facebook-f"></i></a>
+                        <a href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
+                        <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+                <div class="pe">
+                    <img src="<?= base_url(); ?>assets/images/Julie.png" alt="">
                     <div class="p-name">Julie Jamolo</div>
-                    <div class="p-des">Front-end Developer</div>
+                    <div class="p-des">Member</div>
                     <div class="p-sm">
                         <a href="https://www.facebook.com/juliejamolo/"><i class="fab fa-facebook-f"></i></a>
                         <a href="https://twitter.com/itsjuliebird"><i class="fab fa-twitter"></i></a>
@@ -24,23 +34,13 @@
                     </div>
                 </div>
                 <div class="pe">
-                    <img src="<?= base_url(); ?>assets/images/Robert.png" alt="">
-                    <div class="p-name">Robert Lloyd Aban</div>
-                    <div class="p-des">Debugger</div>
+                    <img src="<?= base_url(); ?>assets/images/Ricky.png" alt="">
+                    <div class="p-name">Ricky Martin Roman</div>
+                    <div class="p-des">Member</div>
                     <div class="p-sm">
-                        <a href="https://www.facebook.com/robertkraust"><i class="fab fa-facebook-f"></i></a>
-                        <a href="https://twitter.com/robertowzzz"><i class="fab fa-twitter"></i></a>
-                        <a href="https://www.instagram.com/robertowzzz/"><i class="fab fa-instagram"></i></a>
-                    </div>
-                </div>
-                <div class="pe">
-                    <img src="<?= base_url(); ?>assets/images/Dainielle.png" alt="">
-                    <div class="p-name">Dainielle Sarmiento</div>
-                    <div class="p-des">Researcher</div>
-                    <div class="p-sm">
-                        <a href="https://www.facebook.com/daniloveschicken"><i class="fab fa-facebook-f"></i></a>
-                        <a href="https://twitter.com/dainiii_"><i class="fab fa-twitter"></i></a>
-                        <a href="https://www.instagram.com/_dainiii/"><i class="fab fa-instagram"></i></a>
+                        <a href="https://www.facebook.com/rckyrmn"><i class="fab fa-facebook-f"></i></a>
+                        <a href="https://twitter.com/rckyrmnx"><i class="fab fa-twitter"></i></a>
+                        <a href="https://www.instagram.com/rckyrmnx/"><i class="fab fa-instagram"></i></a>
                     </div>
                 </div>
             </div>
