b19410ecd68fc1ccc69a65941b87cd56d2cc88fe
CWGGa <201812027@fit.edu.ph>
2021-07-14 19:54:01 +0800

    Migrate about page to CI

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -36,6 +36,14 @@ class Home extends CI_Controller
 
     public function about()
     {
+        $user_id = $this->session->userdata('user_id');
+
+        $data = [
+            'header_title' => 'About - Beta Juniors',
+            'main_view' => 'users/about_view'
+        ];
+
+        $this->load->view('layouts/home', $data);
     }
 
     public function logout()
