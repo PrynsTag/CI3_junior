9fd8a0fbc650bda703565319635b0c8a564d1b25
PrynsTag <201812311@fit.edu.ph>
2021-07-16 20:34:23 +0800

    deleted unnecesarry files

--- deleteitem.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'dbconnection.php';
-$sql = "DELETE FROM `posts` WHERE `post_id` = ".$_GET['postid']."";
-$result = $conn->query($sql);
-header('Location: mycollection.php');
-?>
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-	session_start();
-	include('dbconnection.php');
-	session_destroy();
-	header('Location: old_index.php');
-	unset($_SESSION['myid']);
-?>
