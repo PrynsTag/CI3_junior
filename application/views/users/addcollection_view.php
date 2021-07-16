7144c0ff943f41e9f9d1a977833e22dc6522370f
PrynsTag <201812311@fit.edu.ph>
2021-07-16 20:37:21 +0800

    Refactored duplicate code

--- application/views/users/addcollection_view.php
+++ application/views/users/addcollection_view.php
@@ -28,32 +28,29 @@
 <?= isset($error) ? $error : "" ?>
 <div class="d-flex justify-content-center form_container_2">
   <?= form_open_multipart("home/addCollection", array('method' => 'post')) ?>
+
   <div class="input-group mb-3">
     <div class="input-group-append">
       <span class="input-group-text"><i class="fas fa-pager"></i></span>
     </div>
     <?= form_input($input_title); ?>
-    <input type="text" name="title" class="form-control input_text" placeholder="Title">
     <small class="text-danger w-100"><?= form_error("title") ?></small>
   </div>
+
   <div class="input-group mb-3">
     <div class="input-group-append">
       <span class="input-group-text"><i class="fas fa-align-justify"></i></span>
     </div>
     <?= form_textarea($input_description); ?>
-    <textarea class="form-control input_text input_textarea"
-              name="description"
-              placeholder="Description"
-              rows="3">
-    </textarea>
+
     <small class="text-danger w-100"><?= form_error("description") ?></small>
   </div>
+
   <div class="input-group mb-3">
     <div class="input-group-append">
       <span class="input-group-text"><i class="fas fa-file-image"></i></span>
     </div>
     <?= form_upload($input_upload); ?>
-    <input type="file" name="image" class="form-control input_text" id="imgInp"> <br>
     <small class="text-danger w-100"><?= form_error("image") ?></small>
   </div>
   <div class="d-flex justify-content-center login_container">
