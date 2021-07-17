91ddc9de2fcc23b55c940c61d657baee13717836
rckyrmnx <56989333+rckyrmnx@users.noreply.github.com>
2021-07-17 20:54:16 +0800

    Add Image & Form Updated

--- application/views/users/addcollection_view.php
+++ application/views/users/addcollection_view.php
@@ -27,10 +27,11 @@
 <?= isset($alert) ? $alert : "" ?>
 <?= isset($error) ? $error : "" ?>
 <div class="addContainer">
-  <div class="d-flex flex-column justify-content-center form_container_2">
+  <div class="d-flex flex-column justify-content-center form_container_2" style="margin-top:40px">
     <div class="addImageContainer">
-      <img src="<?= base_url("assets/images/default.png"); ?>" class="brand_logo" alt="Logo" id="image_display">
+      <img src="<?= base_url("assets/images/default.png"); ?>" class="addImage" alt="Logo" id="image_display">
     </div>
+    <div class="container-fluid addFormContainer">
       <?= form_open_multipart("home/addCollection", array('method' => 'post')) ?>
       <div class="input-group mb-3">
         <div class="input-group-append">
@@ -79,5 +80,6 @@
         </div>
       </div>
       <?php echo form_close(); ?>
+    </div>
   </div>
 </div>
