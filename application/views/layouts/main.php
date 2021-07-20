f1af68a946dfd4d8806e78c6ea5338228d8b6ab5
rckyrmnx <56989333+rckyrmnx@users.noreply.github.com>
2021-07-20 20:20:05 +0800

    Tab Icon Change

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -8,7 +8,7 @@
   <title><?= $header_title; ?></title>
 
   <!--Icon-->
-  <link rel="icon" href="<?= base_url() ?>assets/images/JARS-ICON-rev.png" type="image/x-icon">
+  <link rel="icon" href="<?= base_url() ?>assets/images/BJ-icon.png" type="image/x-icon">
 
   <!--Bootstrap-->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
